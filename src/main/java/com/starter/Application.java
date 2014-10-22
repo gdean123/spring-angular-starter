9c6230384bf0cf35c5e7cee3252b1ec095a07a25
George Dean <pair+gdean@pivotallabs.com>
2014-10-21 17:48:44 -0700

    Spring application serves static page

---
+++ src/main/java/com/starter/Application.java
@@ -0,0 +1,15 @@
+package com.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.starter")
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
